1a9fc441c5090ef5e616e88f3db84835ef55e1bd
Boscoochen <boscochen203@gmail.com>
2022-04-13 10:58:35 -0500

    resolve comments

--- garage.com/src/test/java/JavaIO.java
+++ garage.com/src/test/java/JavaIO.java
@@ -21,78 +21,39 @@ import org.testng.annotations.Test;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 
+@SuppressWarnings({"all"})
 public class JavaIO {
-	@Test
-	public void openCSVFile() throws IOException, CsvValidationException {
-		//read file using OpenCSV
-		FileInputStream fileInputStream = null;
-		ClassLoader classLoader = this.getClass().getClassLoader();
-		FileReader filereader = new FileReader(new File(classLoader.getResource("basses.csv").getFile()));
-		CSVReader csvReader = new CSVReader(filereader);
-		String[] nextString;
-//		csvReader.readNext();
-		
-		//because hashmap will replace value if pass in the same key
-		//for outputting the info from csv file, hashmap store two arraylist instead
-		HashMap<List<String>, List<String>> hashmap = new HashMap<List<String>, List<String>>();
-		ArrayList<String> arraylist1 = new ArrayList<String>();
-		ArrayList<String> arraylist2 = new ArrayList<String>();
-		while ((nextString = csvReader.readNext()) != null) {
-			for (int i = 0; i < nextString.length; i++) {
-				if (i == 1 || i == 2) {
-					continue;
-				}
-				arraylist1.add(nextString[i]); //list1 store keys
-				arraylist2.add(nextString[i + 1]); //list2 stores values
-			}
-		}
-		
-		//hashmap store two lists
-		hashmap.put(arraylist1, arraylist2);
-		List list1 = new ArrayList();
-		List list2 = new ArrayList();
-		
-		//printout elements
-		Set entrySet = hashmap.entrySet();
-		for (Object object : entrySet) {
-			list1 = ((Map.Entry<List<String>, List<String>>) object).getKey();
-			list2 = ((Map.Entry<List<String>, List<String>>) object).getValue();
-			for(int i = 0; i < arraylist1.size(); i++) {
-				System.out.println(list1.get(i) + " " + list2.get(i));
-			}
-			System.out.println();
-		}
-		
-		
-		Assert.assertEquals(list1.get(0), "Make");
-		Assert.assertEquals(list2.get(0), "Model");
-		
-		Assert.assertEquals(list1.get(1), "Warwick");
-		Assert.assertEquals(list2.get(1), "Corvette");
-		
-		Assert.assertEquals(list1.get(2), "Warwick");
-		Assert.assertEquals(list2.get(2), "Thumb");
-		
-		Assert.assertEquals(list1.get(3), "Warwick");
-		Assert.assertEquals(list2.get(3), "Streamer");
-		
-		Assert.assertEquals(list1.get(4), "Fender");
-		Assert.assertEquals(list2.get(4), "Precision");
-		
-		Assert.assertEquals(list1.get(5), "Fender");
-		Assert.assertEquals(list2.get(5), "Jazz");
-		
-		Assert.assertEquals(list1.get(6), "Yamaha");
-		Assert.assertEquals(list2.get(6), "BB500");
-		
-	
-		
-		
-		
-//		Assert.assertEquals(hashmap.get(arraylist1.get(0)), "Model");
-		
-
-	}
+ @Test
+ public void openCSVFile() throws IOException, CsvValidationException {
+  ClassLoader classLoader = this.getClass().getClassLoader();
+  FileReader filereader = new FileReader(new File(classLoader.getResource("basses.csv").getFile()));
+  CSVReader csvReader = new CSVReader(filereader);
+  String[] nextString;
+  
+  HashMap<String, String> hashmap = new HashMap<String, String>();
+  csvReader.readNext();
+  while ((nextString = csvReader.readNext()) != null) {
+   for (int i = 0; i < nextString.length; i++) {
+    if (i == 1 || i == 2) {
+     continue;
+    }
+    hashmap.put(nextString[i + 1],nextString[i]);
+   }
+  }
+  
+  //printout elements
+  Set entrySet = hashmap.entrySet();
+  for (Object object : entrySet) {
+   String key = ((Map.Entry<String, String>) object).getKey();
+   String value = ((Map.Entry<String, String>) object).getValue();
+   System.out.println(value + " " + key);
+  }
+  
+  Assert.assertEquals(hashmap.get("Streamer"), "Warwick");
+  Assert.assertEquals(hashmap.get("Jazz"), "Fender");
+  Assert.assertEquals(hashmap.get("Precision"), "Fender");
+  Assert.assertEquals(hashmap.get("BB500"), "Yamaha");
+  Assert.assertEquals(hashmap.get("Thumb"), "Warwick");
+  Assert.assertEquals(hashmap.get("Corvette"), "Warwick");
+ }
 }
-
-
