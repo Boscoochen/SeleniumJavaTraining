2a67819f8221d2d622a00cb62830a6e744eaf6e9
Boscoochen <boscochen203@gmail.com>
2022-05-14 00:03:45 -0500

    code fix & clean up

--- garage.com/src/test/java/RestAssured_.java
+++ garage.com/src/test/java/RestAssured_.java
@@ -1,54 +1,14 @@
-
-
 import io.restassured.RestAssured;
 import io.restassured.common.mapper.TypeRef;
-
 import static org.junit.Assert.*;
-
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.junit.Before;
 import org.junit.Test;
-
-
-//import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
-
-/**
- * 
- * API descriptions are on this page: https://reqres.in/
- * 
- * Using RestAssured ... 1. (GET single user) Write a test that verifies the
- * email address for employee id 2 is 'janet.weaver@reqres.in'
- * 
- * 2. (POST login - succesful) Write a test that verifies the token return
- * value.
- * 
- * 3. (DELETE) a. Write a test that verifies the response code of 204. b. Lookup
- * response code 204 and add it to your test as a comment.
- * 
- * 4. (PATCH) Write a test that updates 'Morpheus' to 'Morpheus2'. Ensure that
- * UpdatedAt timestamp is updated and that the name was updated.
- * 
- * 5. (PUT) Write a test that updates 'Morpheus' to 'Morpheus2'. Ensure that
- * UpdatedAt timestamp is updated and that the name was updated.
- * 
- * 6. (GET single user). Deserialize the user data into an object.
- * 
- * 7. (GET list users). Deserialize the user data into a collection of objects.
- * 
- * Use REST Assured to deserialize data. Use Junit as the test framework.
- *
- * 
- */
 
 public class RestAssured_ {
 	@Before
@@ -64,14 +24,10 @@ public class RestAssured_ {
 				.when()
 				.get("/api/users/2")
 				.then()
-//					.log().ifStatusCodeIsEqualTo(200)
-//					.body("result.email", equalTo("janet.weaver@reqres.in"))
 				.extract()
 				.response();
 
-//		String bodyAsString = response.getBody().asString();
-//		System.out.println(bodyAsString);
-		assertEquals(200, response.statusCode());
+		assertEquals(response.statusCode(), 200);
         assertEquals("janet.weaver@reqres.in", response.jsonPath().getString("data.email"));
 	}
 	
@@ -89,11 +45,8 @@ public class RestAssured_ {
 				.extract()
 				.response();
 
-		String bodyAsString = response.getBody().asString();
-//		System.out.println(bodyAsString);
-		assertEquals(200, response.statusCode());
-//		System.out.println(response.jsonPath().getString("token"));
-        assertEquals("QpwL5tke4Pnpja7X4", response.jsonPath().getString("token"));
+		assertEquals(response.statusCode(), 200);
+        assertEquals(response.jsonPath().getString("token"), "QpwL5tke4Pnpja7X4");
 	}
 	
 	@Test
@@ -107,14 +60,12 @@ public class RestAssured_ {
 				.extract()
 				.response();
 		
-		assertEquals(204, response.statusCode());
-//		System.out.println(response.jsonPath().getString("token"));
-//        assertEquals("QpwL5tke4Pnpja7X4", response.jsonPath().getString("token"));
+		assertEquals(response.statusCode(), 204);
 	}
 	
 	@Test
 	public void patchRequestTest() {
-		String jsonData = "{\"name\":\"morpheus2\",\"job\":\"zion resident\"}";
+		String jsonData = "{\"name\":\"morpheus2\"}";
 		Response response = 
 				given()
 				.body(jsonData)
@@ -124,19 +75,15 @@ public class RestAssured_ {
 				.then()
 				.extract()
 				.response();
-		
-		String bodyAsString = response.getBody().asString();
-//		System.out.println(bodyAsString);
-		assertEquals(200, response.statusCode());
-//		System.out.println(response.jsonPath().getString("token"));
-//        assertEquals("QpwL5tke4Pnpja7X4", response.jsonPath().getString("token"));
-		assertEquals("morpheus2", response.jsonPath().getString("name"));
+
+		assertEquals(response.statusCode(), 200);
+		assertEquals(response.jsonPath().getString("name"), "morpheus2");
 	}
 
 	
 	@Test
 	public void putRequestTest() {
-		String jsonData = "{\"name\":\"morpheus2\",\"job\":\"zion resident\"}";
+		String jsonData = "{\"name\":\"morpheus2\"}";
 		Response response = 
 				given()
 				.body(jsonData)
@@ -146,36 +93,90 @@ public class RestAssured_ {
 				.then()
 				.extract()
 				.response();
-		
-//		String bodyAsString = response.getBody().asString();
-//		System.out.println(bodyAsString);
-		assertEquals(200, response.statusCode());
-//		System.out.println(response.jsonPath().getString("name"));
-        assertEquals("morpheus2", response.jsonPath().getString("name"));
+
+		assertEquals(response.statusCode(), 200);
+        assertEquals(response.jsonPath().getString("name"), "morpheus2");
 	}
 	
 	@Test
 	public void deserializeDataIntoObject() {
-		 Object dataObject = 
+		LinkedHashMap<String, Object> actuaLinkedHashMap  = new LinkedHashMap<String, Object>();
+		actuaLinkedHashMap.put("id", 2);
+		actuaLinkedHashMap.put("email", "janet.weaver@reqres.in");
+		actuaLinkedHashMap.put("first_name", "Janet");
+		actuaLinkedHashMap.put("last_name", "Weaver");
+		actuaLinkedHashMap.put("avatar", "https://reqres.in/img/faces/2-image.jpg");
+		
+		Map<String, Map<String, Object>> dataObject = 
 				given()
 				.contentType(ContentType.JSON)
 				.when()
 				.get("/api/users/2")
-				.as(new TypeRef<Object>() {}
-				);
-		System.out.println(dataObject);
+				.as(new TypeRef<Map<String, Map<String, Object>>>() {});
+
+		assertEquals(dataObject.get("data"), actuaLinkedHashMap);
 	}
 	
 	@Test
 	public void deserializeDataIntoCollection() {
-		Map<String, Object> dataObject = 
+		LinkedHashMap<String, Object> actuaLinkedHashMap1  = new LinkedHashMap<String, Object>();
+		actuaLinkedHashMap1.put("id", 7);
+		actuaLinkedHashMap1.put("email", "michael.lawson@reqres.in");
+		actuaLinkedHashMap1.put("first_name", "Michael");
+		actuaLinkedHashMap1.put("last_name", "Lawson");
+		actuaLinkedHashMap1.put("avatar", "https://reqres.in/img/faces/7-image.jpg");
+		
+		LinkedHashMap<String, Object> actuaLinkedHashMap2  = new LinkedHashMap<String, Object>();
+		actuaLinkedHashMap2.put("id", 8);
+		actuaLinkedHashMap2.put("email", "lindsay.ferguson@reqres.in");
+		actuaLinkedHashMap2.put("first_name", "Lindsay");
+		actuaLinkedHashMap2.put("last_name", "Ferguson");
+		actuaLinkedHashMap2.put("avatar", "https://reqres.in/img/faces/8-image.jpg");
+		
+		LinkedHashMap<String, Object> actuaLinkedHashMap3  = new LinkedHashMap<String, Object>();
+		actuaLinkedHashMap3.put("id", 9);
+		actuaLinkedHashMap3.put("email", "tobias.funke@reqres.in");
+		actuaLinkedHashMap3.put("first_name", "Tobias");
+		actuaLinkedHashMap3.put("last_name", "Funke");
+		actuaLinkedHashMap3.put("avatar", "https://reqres.in/img/faces/9-image.jpg");
+		
+		LinkedHashMap<String, Object> actuaLinkedHashMap4  = new LinkedHashMap<String, Object>();
+		actuaLinkedHashMap4.put("id", 10);
+		actuaLinkedHashMap4.put("email", "byron.fields@reqres.in");
+		actuaLinkedHashMap4.put("first_name", "Byron");
+		actuaLinkedHashMap4.put("last_name", "Fields");
+		actuaLinkedHashMap4.put("avatar", "https://reqres.in/img/faces/10-image.jpg");
+		
+		LinkedHashMap<String, Object> actuaLinkedHashMap5  = new LinkedHashMap<String, Object>();
+		actuaLinkedHashMap5.put("id", 11);
+		actuaLinkedHashMap5.put("email", "george.edwards@reqres.in");
+		actuaLinkedHashMap5.put("first_name", "George");
+		actuaLinkedHashMap5.put("last_name", "Edwards");
+		actuaLinkedHashMap5.put("avatar", "https://reqres.in/img/faces/11-image.jpg");
+		
+		LinkedHashMap<String, Object> actuaLinkedHashMap6  = new LinkedHashMap<String, Object>();
+		actuaLinkedHashMap6.put("id", 12);
+		actuaLinkedHashMap6.put("email", "rachel.howell@reqres.in");
+		actuaLinkedHashMap6.put("first_name", "Rachel");
+		actuaLinkedHashMap6.put("last_name", "Howell");
+		actuaLinkedHashMap6.put("avatar", "https://reqres.in/img/faces/12-image.jpg");
+		
+		
+		List<Map<String, Object>>dataObject = 
 				given()
 				.contentType(ContentType.JSON)
 				.when()
 				.get("/api/users?page=2")
-				.as(new TypeRef<Map<String,Object>>() {}
-				);
-		System.out.println(dataObject.get("per_page"));
+				.then()
+				.extract()
+				.path("data");
+
+		assertEquals(dataObject.get(0), actuaLinkedHashMap1);
+		assertEquals(dataObject.get(1), actuaLinkedHashMap2);
+		assertEquals(dataObject.get(2), actuaLinkedHashMap3);
+		assertEquals(dataObject.get(3), actuaLinkedHashMap4);
+		assertEquals(dataObject.get(4), actuaLinkedHashMap5);
+		assertEquals(dataObject.get(5), actuaLinkedHashMap6);	
 	}
 }
 
