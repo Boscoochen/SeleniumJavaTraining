10e930f69ca6dedfde5d4c951a78e3968f2813e5
Boscoochen <boscochen203@gmail.com>
2022-05-27 11:19:56 -0500

    remove comments

--- drivermanagerfactory/src/test/java/DriverTest.java
+++ drivermanagerfactory/src/test/java/DriverTest.java
@@ -1,17 +1,11 @@
 import static org.testng.Assert.assertNotNull;
 import org.testng.annotations.Test;
 public class DriverTest extends TestBase{
-	/**
-	 * Change browserType=edge in config.properties before test
-	 */
 	@Test
 	public void canLaunchEdgeDriver() {
-		assertNotNull(this.getEdgeDriverManager().getDriver(),"the webdriver should be launch in the super class.");
+		assertNotNull(this.getEdgeDriverManager().getDriver(),"the edgedriver should be launch in the super class.");
 	}
 	
-	/**
-	 * Change browserType=chrome in config.properties before test
-	 */
 	@Test
 	public void canLaunchChromeDriver() {
 		assertNotNull(this.getChromeDriverManager().getDriver(),"the chromedriver should be launch in the super class.");
