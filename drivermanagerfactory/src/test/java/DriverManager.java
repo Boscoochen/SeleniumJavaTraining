1db5f5f56e37d7c53ff58b92df1e14e5d2fa10cc
Boscoochen <boscochen203@gmail.com>
2022-05-30 18:25:12 -0500

    fix request changes

--- drivermanagerfactory/src/test/java/DriverManager.java
+++ drivermanagerfactory/src/test/java/DriverManager.java
@@ -23,29 +23,6 @@ public abstract class DriverManager extends DriverManagerFactory {
 	public void loadWebsite(String webSite) {
 		this.driver.get(webSite);
 	}
-	
-	protected String getDriverPath(String browserName) {
-		String path = null;
-		String osName = osName();
-		if (osName.equals("mac os x")) {
-			if (browserName.toLowerCase().equals("chrome")) {
-				path = "src/test/resources/chromedriver";
-			} else if (browserName.toLowerCase().equals("edge")) {
-				path = "src/test/resources/msedgedriver";
-			}
-		} else if (osName.equals("windows 11")) {
-			if (browserName.toLowerCase().equals("chrome")) {
-				path = "src/test/resources/chromedriver.exe";
-			} else if (browserName.toLowerCase().equals("edge")) {
-				path = "src/test/resources/msedgedriver.exe";
-			}
-		}
-		return path;
-	}
-
-	protected String osName() {
-		return System.getProperty("os.name").toLowerCase();
-	}
 
 	public void quitDriver() {
 		if(this.driver != null) {
--- drivermanagerfactory/src/test/java/TestBase.java
+++ drivermanagerfactory/src/test/java/TestBase.java
@@ -34,6 +34,29 @@ public abstract class TestBase {
 			this.edgeDriverManager.quitDriver();
 		}
 	}
+	
+	protected String getDriverPath(String browserName) {
+		String path = null;
+		String osName = osName();
+		if (osName.equals("mac os x")) {
+			if (browserName.toLowerCase().equals("chrome")) {
+				path = "src/test/resources/chromedriver";
+			} else if (browserName.toLowerCase().equals("edge")) {
+				path = "src/test/resources/msedgedriver";
+			}
+		} else if (osName.equals("windows 11")) {
+			if (browserName.toLowerCase().equals("chrome")) {
+				path = "src/test/resources/chromedriver.exe";
+			} else if (browserName.toLowerCase().equals("edge")) {
+				path = "src/test/resources/msedgedriver.exe";
+			}
+		}
+		return path;
+	}
+
+	protected String osName() {
+		return System.getProperty("os.name").toLowerCase();
+	}
 
 	public ChromeDriverManager getChromeDriverManager() {
 		return this.chromeDriverManager;
